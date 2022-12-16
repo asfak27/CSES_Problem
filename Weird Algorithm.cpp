3d49e468ad5f0095508ece867f90643e738eb28c
Asfak Shahrier <59991886+asfak27@users.noreply.github.com>
2022-12-16 19:48:43 +0600

    Create Weird Algorithm.cpp

---
+++ Weird Algorithm.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Asfak_Shahrier//
+#define  ll long long
+#define  no               cout<<"No"<<endl
+#define  yes              cout<<"Yes"<<endl
+#define  mod       1000000007
+#define  N        1002
+#define  fast  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+void solve()
+{
+    map<ll,ll>mp,mp1;
+    vector<ll>v;
+    ll n,m,j,i,y,z,l,r,x,k;
+    cin>>n;
+    cout<<n<<" ";
+    while(n>1)
+    {
+        if(n%2==1)
+        {
+            n*=3;
+            n++;
+        }
+        else {
+            n/=2;
+        }
+        cout<<n<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    fast;
+    int T=1;
+    //cin>>T;
+    while(T--)solve();
+    return 0;
+}
